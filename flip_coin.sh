f643f64138a9f807f7d9891ae7c0c502ff9b4143
prem-12345 <premkpatil1997@gmail.com>
2021-06-04 22:21:15 +0530

    second commit

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 #"DAY 10:FLIP COIN SIMULATION PROBLEM"
+
+#UC_1:SIMULATOR START FLIPPING COIN TO DISPLAY HEADS OR TAILS AS WINNER
+
+flip=$((RANDOM%2))
+
+if [[ $flip -eq 0 ]]
+then
+	echo "Head Wins"
+else
+	echo "Tail Wins"
+fi
