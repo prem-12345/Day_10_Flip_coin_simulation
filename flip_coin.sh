b272add6880bd13ef8079d0eba0877505b5fb22b
prem-12345 <premkpatil1997@gmail.com>
2021-06-04 23:18:27 +0530

    fourth commit

--- flip_coin.sh
+++ flip_coin.sh
@@ -16,7 +16,7 @@ fi
 #UC_2:SHOW NUMBER OF TIMES HEADS OR TAILS WIN
 
 
-for ((i=0;i<=$n;i++))
+for ((i=0;i<=n;i++))
 do
 	flip=$((RANDOM%2))
 if [ $flip -eq 0 ]
@@ -25,4 +25,29 @@ then
 else
 	echo "Tail wins"
 fi
-	
+done	
+
+#UC_3:EITHER HEAD OR TAIL WON 21 TIMES
+
+head=0
+tail=0
+
+while [ $head -ne 21 ] && [ $tail -ne 21 ]
+do
+	flip=$((RANDOM%2))
+
+if	[ $flip -eq 0 ]
+then
+	head=$(($head+1))
+else
+	tail=$(($tail+1))
+fi
+done
+
+if [ $head -eq 21 ]
+then
+	echo "head win" $head "times"
+else
+	echo "tail win" $tail "times"
+fi
+
